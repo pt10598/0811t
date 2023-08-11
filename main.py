ac18a185413e88df966c71e3ed4af2b9493810e2
pt10598 <130521177+pt10598@users.noreply.github.com>
2023-08-11 16:12:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def submit():
         user = request.form['user']
         tte = request.form['user2']
         tte2 = request.form['user3']
+        chrome_options=Options()
+        chrome_options.add_argument("--headless")
+        driver=webdriver.Chrome(options=chrome_options)
+        driver.get("https://ppstrq.nat.gov.tw/pps/pubQuery/PropertyQuery/propertyQuery.do")
         return redirect(url_for('success', name=user,serc6=tte,serc9=tte2))
 
 @app.route('/success/<name>/<serc6>/<serc9>/')
